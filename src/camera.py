05b0abd048cd4ec6dd83b0f73298d6f35e5a09a2
qmo1222 <howard19192828@gmail.com>
2019-01-06 19:26:18 +0800

    fix seg fault

--- src/camera.py
+++ src/camera.py
@@ -6,10 +6,13 @@ This file contains classes for handling camera
 from PyQt5.QtCore import QThread
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5 import QtCore
+from PIL.ImageQt import ImageQt
+from PIL import Image
 from filters import *
 import cv2
 import time
 import os
+
 
 class CameraThread(QThread):
     imgLab = None
@@ -28,22 +31,24 @@ class CameraThread(QThread):
             try:
                 while True:
                     _, frame = self.device.read()
-                    # convert from BGR to RGB 
+                    # convert from BGR to RGB
                     cv2.cvtColor(frame, cv2.COLOR_BGR2RGB, frame)
                     # frame = blur(frame)
                     # frame = gray_scale(frame).astype('uint8')
                     # convert to QImage
                     if self.output.not_in_use:
                         self.output.not_in_use = False
-                        self.output.data = frame.data
+                        self.output.data = frame
+                        # import pdb
+                        # pdb.set_trace()
                         self.output.not_in_use = True
-                    
+
             finally:
                 self.device.release()
 
     def destroyed(self, QObject=None):
         self.device.release()
-        
+
     def save_frame(self, path=None):
         if path is None:
             name = 'img-'+str(int(time.time()))+'.png'
@@ -62,29 +67,31 @@ class FrameThread(QThread):
     WIDTH = 1280
     HEIGHT = 720
     CHANNEL = 3
-    
+
     def __init__(self, img_lab, i_frame):
         QThread.__init__(self)
         self._img_lab = img_lab
         self.input_frame = i_frame
-    
+
     def run(self):
         try:
             while True:
                 # print(self.input_frame)
-                if (self.input_frame.data is not None) and (self.input_frame.not_in_use) :
+                if (self.input_frame.data is not None) and (self.input_frame.not_in_use):
                     self.input_frame.not_in_use = False
                     if self._filter_enable:
                         pass
                     else:
                         # frame = self.input_frame.data
-                        image = QImage(self.input_frame.data,
-                                       self.WIDTH,
-                                       self.HEIGHT,
-                                       self.CHANNEL*self.WIDTH, 
-                                       QImage.Format_RGB888)
+                        # image = QImage(self.input_frame.data,
+                        #                self.WIDTH,
+                        #                self.HEIGHT,
+                        #                QImage.Format_RGB888)
+                        image = Image.fromarray(self.input_frame.data)
+                        image = ImageQt(image)
                         pixmap = QPixmap.fromImage(image)
-                        pixmap = pixmap.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
+                        pixmap = pixmap.scaled(
+                            640, 480, QtCore.Qt.KeepAspectRatio)
                         self._img_lab.setPixmap(pixmap)
                         del image
                         del self.input_frame.data
